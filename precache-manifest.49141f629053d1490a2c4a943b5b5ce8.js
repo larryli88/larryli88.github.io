6205ed975b199fa0b2df19d9f475ce0737c85ff3
Larry <yuetao1998@gmail.com>
2020-07-09 22:47:39 -0500

    Updates

---
+++ precache-manifest.49141f629053d1490a2c4a943b5b5ce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e3b28f057145b0c112f3c47973db2d",
+    "url": "/personal-page/index.html"
+  },
+  {
+    "revision": "7a23a1dd2d40e5bf7c35",
+    "url": "/personal-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b0d75ee245d49f66ca5",
+    "url": "/personal-page/static/js/2.bf25e480.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-page/static/js/2.bf25e480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a23a1dd2d40e5bf7c35",
+    "url": "/personal-page/static/js/main.878c9b08.chunk.js"
+  },
+  {
+    "revision": "ae1c9ec924b7c4819ed0",
+    "url": "/personal-page/static/js/runtime-main.244682c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
